9ed8a7f06406ea3f820fba48fbdfac2b3203b891
Daniel Jordan <103155866+SecretSocietyTM@users.noreply.github.com>
2025-12-31 14:00:07 -0800

    log: scrap previous work
    I decided that trying to write my own 2d thing (I don't
    event know what its called) was a waste of time since
    I could achieve the same thing with HTML's built in
    svg element and I could make use of 20+ years of development
    to CSS to style things the way I want. This way I can
    get to designing the page faster and actually coding
    the interactability for the DSA visualizations.

---
+++ main.js
@@ -0,0 +1,13 @@
+// I've decided to scrap all the previous work
+//
+// I will instead make use of HTML svg instead.
+// Doing so will hopefully increase the speed at which I 
+// progress in the actual DSA part of the project because 
+// at the moment I am spending way too much time just working
+// on the graphics / canvas2d part of the project which is 
+// a massive waste of time.
+//
+//
+//
+// Now I have to go through and learn the svg library more...
+// I suppose I can start making a rough design on figma!

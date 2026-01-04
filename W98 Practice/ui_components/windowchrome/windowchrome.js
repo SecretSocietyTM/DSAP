a0df56ab7bd4fba9ea8704115fb10aae7348241c
Daniel Jordan <103155866+SecretSocietyTM@users.noreply.github.com>
2026-01-04 00:37:07 -0800

    log: rework w98 css files

--- W98 Practice/ui_components/windowchrome/windowchrome.js
+++ W98 Practice/ui_components/windowchrome/windowchrome.js
@@ -4,3 +4,16 @@ const last_child = dom.children[dom.children.length - 1];
 last_child.style["border"] = "none";
 last_child.style["margin"] = "0px";
 console.log(last_child)
+
+
+/* 
+Testing new method.
+*/
+const dom2 = document.querySelectorAll(".w98-windowchrome");
+console.log(dom2);
+
+dom2.forEach(parent => {
+    const last_child = parent.children[parent.children.length - 1];
+    last_child.style["margin"] = "0px";
+    last_child.style["box-shadow"] = "none";
+});
